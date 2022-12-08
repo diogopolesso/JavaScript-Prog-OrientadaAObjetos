78287b077655666339d5e6cedf32c34cc57b7e68
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-08 18:15:21 -0300

    Atributos Privados

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ import Docente from "./Docente.js";
 //const novoUser = new User ('Mariana', 'mariana@uol.com', '2021/04/05');
 //console.log(novoUser.exibirInfos());
 
-const novoAdmin = new Admin('Rodrigo', 'rodrigo@uol.com', '2021-01-01');
-console.log(novoAdmin.nome);
-novoAdmin.nome = '';
-console.log(novoAdmin.nome);
+// const novoAdmin = new Admin('Rodrigo', 'rodrigo@uol.com', '2021-01-01');
+// console.log(novoAdmin.nome);
+// novoAdmin.nome = '';
+// console.log(novoAdmin.nome);
+
+const novoDocente = new Docente('Guilherme', 'guilherme@uol.com', '2011/01/03')
+console.log(novoDocente.exibirInfos());
