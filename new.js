ce6191d77117170ab5abb7d49f4eaf38da3ae0ba
diogopolesso <diogopolesso@gmail.com>
2022-12-08 14:41:14 -0300

    Object.create() vs New

---
+++ new.js
@@ -0,0 +1,45 @@
+// NEW Função Construtora que equivale a Classes em Java Script:
+
+/*
+function User(nome, email){
+    this.nome = nome
+    this.email = email
+
+    this.exibirInfos = function(){
+        return `${this.nome}, ${this.email}`
+    }
+}
+
+const novoUser = new User('Juliana', 'juliana@uol.com')
+console.log(novoUser.exibirInfos());
+
+function Admin(role){
+    User.call(this, 'Juliana', 'juliana@uol.com')
+    this.role = role || 'estudante'
+}
+
+Admin.prototype = Object.create(User.prototype)
+const novoUser = new Admin('Admin')
+console.log(novoUser.exibirInfos())
+console.log(novoUser.role)
+*/
+
+// Formato de Objeto:
+
+const user = {
+    init: function (nome, email){
+        this.nome = nome
+        this.email = email
+    },
+    
+    exibirInfos: function(nome){
+        return this.nome
+    }
+}
+
+const novoUser = Object.create(user);
+novoUser.init('Juliana', 'juliana@uol.com');
+console.log(novoUser.exibirInfos());
+
+// Testar se User esta sendo usado como Protótipo de novoUser:
+console.log(user.isPrototypeOf(novoUser));
